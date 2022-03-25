c9da9641a88f62fc0bd188799566fe601e6da548
WardPearce <wardpearce@protonmail.com>
2022-03-26 00:39:37 +1300

    Removed run_dev.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from paycord import app
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, port=8888)
--- run_dev.py
+++
@@ -1,5 +0,0 @@
-from app.main import app
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=8888)
