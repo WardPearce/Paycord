e80b854417ffd40e44368e341b4597e709d6b0dd
WardPearce <wardpearce@protonmail.com>
2022-03-26 01:20:00 +1300

    Should be using os.environ

--- paycord/env.py
+++ paycord/env.py
@@ -2,13 +2,13 @@ import os
 from currency_symbols import CurrencySymbols
 
 
-DISCORD_API_URL = os.getenv("DISCORD_API_URL")
-CURRENCY = os.getenv("CURRENCY")
+DISCORD_API_URL = os.environ["DISCORD_API_URL"]
+CURRENCY = os.environ["CURRENCY"]
 CURRENCY_SYMBOL = CurrencySymbols.get_symbol(CURRENCY)
-PAGE_NAME = os.getenv("PAGE_NAME")
-LOGO_URL = os.getenv("LOGO_URL")
+PAGE_NAME = os.environ["PAGE_NAME"]
+LOGO_URL = os.environ["LOGO_URL"]
 ROOT_DISCORD_IDS = os.environ["ROOT_DISCORD_IDS"].split(",")
-SUBSCRIPTION_RECURRENCE = os.getenv("SUBSCRIPTION_RECURRENCE")
+SUBSCRIPTION_RECURRENCE = os.environ["SUBSCRIPTION_RECURRENCE"]
 SUBSCRIPTION_INTERVAL = int(os.environ["SUBSCRIPTION_INTERVAL"])
 MONTHLY_GOAL = float(os.environ["MONTHLY_GOAL"])
 MONTHLY_GOAL_PARAGRAPH = os.environ["MONTHLY_GOAL_PARAGRAPH"]
